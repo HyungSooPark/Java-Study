bdb4d68935fe0830e61040f9b20cb1b4da3f9a68
HyungSooPark <rhdwkd92@gmail.com>
2020-04-12 17:05:56 +0900

    13.Polymorphism

--- Java13_Polymorphism/src/com/poly/silsub1/shape/run/Run.java
+++ Java13_Polymorphism/src/com/poly/silsub1/shape/run/Run.java
@@ -3,7 +3,7 @@ package com.poly.silsub1.shape.run;
 import com.poly.silsub1.shape.controller.ShapeManager;
 
 public class Run {
-	//실행용 클래스
+
 	public static void main(String[] args) {
 		ShapeManager s = new ShapeManager();
 		
