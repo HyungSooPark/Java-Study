261e1c99a9a66e0db7608fdd95fa557fb4a7f686
HyungSooPark <rhdwkd92@gmail.com>
2020-04-12 16:54:45 +0900

    00.firstProject

---
+++ firstProject/src/firstProject/com/test/Hello.java
@@ -0,0 +1,9 @@
+package firstProject.com.test;
+
+public class Hello {
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		/*System.out.println("점심 시간이 다가옵니다.");
+		System.out.println("배가 고프네요./.");*/
+	}
+}
