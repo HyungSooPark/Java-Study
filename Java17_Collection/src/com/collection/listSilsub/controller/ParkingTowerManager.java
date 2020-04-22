8c6963183c6fbf8389a9b31ef628121b64268d36
HyungSooPark <rhdwkd92@naver.com>
2020-04-22 15:08:56 +0900

    17.Collection

--- Java17_Collection/src/com/collection/listSilsub/controller/ParkingTowerManager.java
+++ Java17_Collection/src/com/collection/listSilsub/controller/ParkingTowerManager.java
@@ -1,6 +1,7 @@
 package com.collection.listSilsub.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.collection.listSilsub.model.Car;
 
@@ -28,5 +29,33 @@ public class ParkingTowerManager {
 	
 	public ArrayList<Car> selectList(){
 		return carList;
+	}
+	
+	public int deleteCar(int carNum) {
+		Car del = null;
+		for(int i=0;i<carList.size();i++) {
+			if(carList.get(i).getCarNum()==carNum) {
+				del = carList.remove(i);
+			}
+		}
+		
+		if(del!=null) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public List<Car> searchCar(String owner) {
+		List<Car> searchList = new ArrayList<>();
+		
+		for(int i=0;i<carList.size();i++) {
+			if(carList.get(i).getOwner().equals(owner)) {
+				searchList.add(carList.get(i));
+			}
+		}
+		
+		return searchList;
 	}
 }
--- Java17_Collection/src/com/collection/listSilsub/view/ParkingTowerMenu.java
+++ Java17_Collection/src/com/collection/listSilsub/view/ParkingTowerMenu.java
@@ -1,6 +1,7 @@
 package com.collection.listSilsub.view;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.collection.listSilsub.controller.ParkingTowerManager;
@@ -19,6 +20,8 @@ public class ParkingTowerMenu {
 			System.out.println("=== *주차 타워 관리* ===");
 			System.out.println("1. 차량 주차");
 			System.out.println("2. 전체 출력");
+			System.out.println("3. 차량 출차");
+			System.out.println("4. 차량 검색");
 			System.out.println("0. 끝내기");
 			System.out.println();
 			System.out.print("메뉴 선택: ");
@@ -31,6 +34,12 @@ public class ParkingTowerMenu {
 			case 2:
 				selectList();
 				break;
+			case 3:
+				deleteCar();
+				break;
+			case 4:
+				searchCar();
+				break;
 			case 0:
 				System.out.print("프로그램 종료");
 				return;
@@ -42,15 +51,16 @@ public class ParkingTowerMenu {
 		}
 		
 	}
-	
+
 	public void insertCar() {
 		System.out.println();
 		System.out.print("차량 번호 : ");
 		int carNum = sc.nextInt();
 		System.out.print("차량 타입 (1:경차 / 2:세단 / 3:SUV / 4:트럭) : ");
 		int carType = sc.nextInt();
+		sc.nextLine();
 		System.out.print("차주 : ");
-		String owner = sc.next();
+		String owner = sc.nextLine();
 		
 		Car car = new Car(carNum,carType,owner);
 		ptm.insertCar(car);
@@ -63,6 +73,37 @@ public class ParkingTowerMenu {
 		else {
 			for(int i=0;i<carList.size();i++) {
 				System.out.println(carList.get(i));
+			}
+		}
+	}
+	
+	public void deleteCar() {
+		System.out.println();
+		System.out.print("차량 번호 : ");
+		int carNum = sc.nextInt();
+		int res = ptm.deleteCar(carNum);
+		System.out.println();
+		if(res>0) {
+			System.out.println("차량 출차 완료!");
+		}
+		else {
+			System.out.println("입력하신 차량이 주차되어 있지 않습니다.");
+		}
+	}
+	
+	private void searchCar() {
+		System.out.println();
+		sc.nextLine();
+		System.out.print("주차 차량 검색(차주 입력) : ");
+		String owner = sc.nextLine();
+		List<Car> searchList = ptm.searchCar(owner);
+		System.out.println();
+		if(searchList.isEmpty()) {
+			System.out.println("검색한 차량이 존재하지 않습니다.");
+		}
+		else {
+			for(int i=0;i<searchList.size();i++) {
+				System.out.println(searchList.get(i));
 			}
 		}
 	}
