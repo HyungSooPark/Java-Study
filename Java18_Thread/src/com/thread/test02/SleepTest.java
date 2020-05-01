964e0274107c58e0cab68ea7d669f6ae2a37cfeb
HyungSooPark <rhdwkd92@naver.com>
2020-05-01 13:02:16 +0900

    18.Thread

--- Java18_Thread/src/com/thread/test02/SleepTest.java
+++ Java18_Thread/src/com/thread/test02/SleepTest.java
@@ -3,9 +3,9 @@ package com.thread.test02;
 public class SleepTest {
 
 	public static void main(String[] args) {
-		for(int i=0;i<50;i++) {
+		for(int i=0;i<5;i++) {
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(600);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
